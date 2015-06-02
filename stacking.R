ec7f5f2ea322649af19a13e32286c55742387d74
samtisi <samtisi@gmail.com>
2015-06-02 13:31:55 -0400

    first

---
+++ stacking.R
@@ -0,0 +1,11 @@
+library(caret)
+library(gbm)
+set.seed(3433)
+library(AppliedPredictiveModeling)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+set.seed(62433)
+
